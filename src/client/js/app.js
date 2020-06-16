18280895bc04542060d3baa96bb90d1dd6e9b3d5
tinku parmar <tinkuparmar99@gmail.com>
2020-06-16 10:14:45 +0530

    fetch geonames

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -7,19 +7,39 @@ const generate = document.getElementById('generate');
 let d = new Date();
 let newDate = d.getDate() + '.' + d.getMonth() + '.' + d.getFullYear();
 
-function handleSubmit() { generate.addEventListener('click', ()=>{
-  const result = fetch(`http://localhost:8081/cities/`,{
-    method:'POST',
-    credentials: "same-origin",
+const getTrips = async () => {
+  try {
+    const result = await fetch('http://localhost:8081/trips')
+    const trips = await result.json()
+    if (trips.length > 0) {
+      trips.forEach(trip => console.log(trip))
+    }
+  } catch (e) {
+    setError("We couldn't fetch your trips. Please try again later.")
+  }
+}
+
+const saveTrip = async (location, date) => {
+  const result = await fetch('http://localhost:8081/trip', {
+    method: 'POST',
     mode: 'cors',
     headers: {
-        'Content-Type': 'application/json',
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({term: city.value})
+    body: JSON.stringify({location, date}),
   })
-   .then(res => console.log(res))
+  if (!result.ok) {
+    setFormError("We weren't able to save your trip. Please try again.")
+  } else {
+    getTrips()
+  }
+}
+
+function handleSubmit() { generate.addEventListener('click', ()=>{
+  saveTrip(city.value, date.value);
+  
 })
 }
 
-//handleSubmit();
+handleSubmit();
 export {handleSubmit}
--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
 //empty js object which act as endpoint for all routes
-//projectData = {};
+tripData = []
 
 //require express bodyParser and cors
 const express = require('express');
@@ -12,14 +12,14 @@ dotenv.config();
 const app = express();
 
 //geonames
-const usernameGeo = process.env.username;
-const baseurlGeo = process.env.geoBaseUrl;
+// const usernameGeo = process.env.username;
+// const baseurlGeo = process.env.geoBaseUrl;
 
-//weatherbit
-const weatherbitkey = process.env.weatherBitApi;
+// //weatherbit
+// const weatherbitkey = process.env.weatherBitApi;
 
-//pixabay 
-const pixabaykey = process.env.pixabayApi
+// //pixabay 
+// const pixabaykey = process.env.pixabayApi
 
 //bodyParser as a middleware
 app.use(bodyParser.urlencoded({extended: false}));
@@ -37,18 +37,56 @@ app.get('/', (req, res) => {
     res.sendFile('dist/index.html');
 })
 
-//get city
-app.post('/cities', async(req, res)=>{
-    try{
-        const city = req.body.term
-        const result = await fetch(`${baseurlGeo}q=${city}&maxRows=10&username=${usernameGeo}`)
-        const cities = await result.json()
-        console.log(cities)
-    } catch(error){
-        throw error 
-    }
-    res.send(cities)
+app.get('/trips', (req, res) => {
+    res.status(200).send(tripData)
 })
+//http://api.geonames.org/searchJSON?q=london&maxRows=10&username=demo
+//fetching data through geonames api
+const fetchCityData = async (city) => {
+    try {
+      if (!city) {
+        throw 'Please provide a city!'
+      }
+      const result = await fetch(
+        `http://api.geonames.org/searchJSON?q=${city}&maxRows=10&username=tinkuparmar`,
+      )
+      
+      const {geonames: cities} = await result.json()
+      console.log(cities)
+      if (cities.length > 0) {
+        const location = {
+          location: `${cities[0].name}`,
+          lat: cities[0].lat,
+          lng: cities[0].lng,
+        }
+        return location
+      }
+      return {}
+    } catch (e) {
+      throw e
+    }
+  }
+
+
+//get city
+app.post('/trip', async (req, res) => {
+    try {
+        const city = req.body.location
+        const newdate = req.body.date
+      const [lat, lng, location] = await fetchCityData(city)
+      const trip = {
+        lat,
+        lng,
+        location,
+        newdate,
+      }
+      tripData.push(trip)
+      res.status(201).send()
+    } catch (e) {
+      console.log(e)
+      res.sendStatus(404)
+    }
+  })
 
 //server setup
 app.listen(8081, ()=>{
